048ed55d2a83edcfb4e8a6987a190a08ee4ab401
FernandoDenis <Supergogleyoutu@gmail.com>
2024-08-17 00:44:53 -0600

    Se modifico el archivo de heaps para hacer las modificaciones mencionadas

--- heap/usandoHeaps.py
+++ heap/usandoHeaps.py
@@ -8,21 +8,19 @@ import random
 #Array donde van a estar todas las alarmas
 alarmas = []
 
-#Array donde van a estar solo las alarmas que no hayan pasado
-restAlarmas = []
-
-#Variable donde se guarda el epoch actual para poder trabajar con el mismo valor al crear las alarmas
-epoch_actual = int(time.time())
-
 #Ciclo para agregar 10,000 alarmas aleatorias y agregarlas en el array "alarmas"
 for i in range(10000):
-    #Se suma un numero random entre un rango de -+100,000 para que agregue alarmas que ya hayan pasado y otras que no
-    alarmas.append(epoch_actual + random.randint(-100000,100000))
+    #Estructura que suma un numero random entre un rango de -+100,000 para que agregue alarmas que ya hayan pasado y otras que no
+    alarma = (f"Alarma{i}", int(time.time()) + random.randint(-100000,100000))
+    alarmas.append(alarma)
 
-#Ciclo para eliminar las alarmas que ya hayan pasado y solo queden las que faltan en el futuro
-for i in alarmas:
-    if (i - epoch_actual) >= 0:
-        restAlarmas.append(i)
 
-#Heap para mostrar las proximas 5 alarmas que van a sonar en un futuro
-print(heapq.nsmallest(5, restAlarmas))
+#Ordena de menor a mayor el array alarmas para tener control del tiempo de las alarmas
+alarmas = heapq.nsmallest(len(alarmas), alarmas, key=lambda x: x[1])
+
+# #Saca las alarmas hasta llegar a la del tiempo actual
+while(alarmas[0][1] < int(time.time())):
+    alarmas.pop(0)
+
+#Imprime las 5 alarmas que siguen
+print(heapq.nsmallest(5,alarmas,key=lambda x: x[1]))
