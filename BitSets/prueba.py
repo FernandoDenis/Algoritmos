efdfde0c1d2296c53558d0403ff972b5ef2c7c8f
FernandoDenis <Supergogleyoutu@gmail.com>
2024-09-15 13:20:46 -0600

    Ejercicio de bitsets junto con su uso en prueba

---
+++ BitSets/prueba.py
@@ -0,0 +1,15 @@
+
+from usandoBitsets import BitVector
+
+prueba = BitVector(70000000)
+
+print("Empieza")
+
+with open('BitSets/output.txt', 'r') as archivo:
+    for linea in archivo:
+        numero = linea.strip()
+        numero = int(numero)
+        prueba.set(numero)
+
+    print(bin(prueba.vector[0]))
+    print(prueba.foundUnique())
