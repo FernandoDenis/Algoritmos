9fc064fa8a49d073198574c5727156dcfd27b0c5
FernandoDenis <Supergogleyoutu@gmail.com>
2024-12-03 21:03:02 -0600

    cache funcionando

--- Cache/cache-LRU.py
+++ Cache/cache-LRU.py
@@ -8,6 +8,8 @@ class LRUCache:
         self.cache = dict()
         self.limit = 7
         self.memory = []
+        # indice para asignar indice al mapa
+        self.idx = 0
         
     def get(self, id, value):
         # Aseguramos que el id no se encuentre ya en el cache
@@ -17,41 +19,80 @@ class LRUCache:
         
         # Revisamos si el cache esta lleno o no, para poder agregar elementos sin tener que quitar ninguo
         if len(self.memory) < self.limit:
-            self.cache[id] = value
-            self.memory.append((id, time.time()))
+            self.cache[id] = self.idx
+            self.idx += 1
+            self.memory.append((value, time.time(), id))
             return        
         else: 
             # Eliminamos el elemento mas viejo
-            self.memory.pop(0)
+            delElement = self.memory.pop(0)
+            # Elimino el elemento del cache por medio de su id
+            del self.cache[delElement[2]]
             
-            # Agregamos el nuevo elemento en la posicion donde no hay elementos
-            self.memory.insert(0 ,(id, time.time()))
+            # Agregamos el nuevo elemento en la posicion donde quitamos el elementos
+            self.memory.insert(0 ,(value, time.time(), id))
+            # Se agrega el nuevo elemento al cache
+            self.cache[id] = 0
             
             # Se decide que hijo es el que lleva mas tiempo y lo movemos hasta adelante ya que es el mas reciente
             if self.memory[1][1] > self.memory[2][1]:
+                # Cambiamos de posicion en el cache usando los ids
+                self.cache[self.memory[0][2]], self.cache[self.memory[2][2]] = self.cache[self.memory[2][2]],  self.cache[self.memory[0][2]]
+                # Cambio de posicion en la memoria (array)
                 self.memory[0],self.memory[2] = self.memory[2], self.memory[0]
                 idxActual = 2
             else:
+                self.cache[self.memory[0][2]], self.cache[self.memory[1][2]] = self.cache[self.memory[1][2]], self.cache[self.memory[0][2]]
+                
                 self.memory[0],self.memory[1] = self.memory[1], self.memory[0]
                 idxActual = 1
             
+            # Se le suma 1 al indice actual para arreglar el problema de la formula cuando el indice es 1 (1  * 2 = 2 (2 NO ES EL HIJO DE 1))
             while ((idxActual + 1) * 2) < self.limit:
                 if self.memory[((idxActual + 1) * 2) - 1][1] > self.memory[(idxActual + 1) * 2 ][1]:
+                    self.cache[self.memory[idxActual][2]], self.cache[self.memory[(idxActual + 1) * 2][2]] = self.cache[self.memory[(idxActual + 1) * 2][2]], self.cache[self.memory[idxActual][2]]
+                
                     self.memory[idxActual],self.memory[(idxActual + 1) * 2] = self.memory[(idxActual + 1) * 2], self.memory[idxActual]
                     idxActual = (idxActual + 1) * 2
                 else:
+                    self.cache[self.memory[idxActual][2]], self.cache[self.memory[((idxActual + 1) * 2) - 1][2]] = self.cache[self.memory[((idxActual + 1) * 2) - 1][2]], self.cache[self.memory[idxActual][2]]
+                
                     self.memory[idxActual],self.memory[((idxActual + 1) * 2) - 1] = self.memory[((idxActual + 1) * 2) - 1], self.memory[idxActual]
                     idxActual = ((idxActual + 1) * 2) - 1
+        return
 
     def update(self, id):
         if id in self.cache:
-            for data in self.memory:
-                if id == data[1]:
-                    data = (id, time.time())
-                    return
-            return
+            
+            # Guardar el indice del id que se quiere buscar
+            idxActual = self.cache[id]
+            
+            if idxActual == 0:
+                if self.memory[1][1] > self.memory[2][1]:
+                    # Cambiamos de posicion en el cache usando los ids
+                    self.cache[self.memory[0][2]], self.cache[self.memory[2][2]] = self.cache[self.memory[2][2]],  self.cache[self.memory[0][2]]
+                    # Cambio de posicion en la memoria (array)
+                    self.memory[0],self.memory[2] = self.memory[2], self.memory[0]
+                    idxActual = 2
+                else:
+                    self.cache[self.memory[0][2]], self.cache[self.memory[1][2]] = self.cache[self.memory[1][2]], self.cache[self.memory[0][2]]
+                
+                    self.memory[0],self.memory[1] = self.memory[1], self.memory[0]
+                    idxActual = 1
+            
+            while ((idxActual + 1) * 2) < self.limit:
+                if self.memory[((idxActual + 1) * 2) - 1][1] > self.memory[(idxActual + 1) * 2 ][1]:
+                    self.cache[self.memory[idxActual][2]], self.cache[self.memory[(idxActual + 1) * 2][2]] = self.cache[self.memory[(idxActual + 1) * 2][2]], self.cache[self.memory[idxActual][2]]
+                
+                    self.memory[idxActual],self.memory[(idxActual + 1) * 2] = self.memory[(idxActual + 1) * 2], self.memory[idxActual]
+                    idxActual = (idxActual + 1) * 2
+                else:
+                    self.cache[self.memory[idxActual][2]], self.cache[self.memory[((idxActual + 1) * 2) - 1][2]] = self.cache[self.memory[((idxActual + 1) * 2) - 1][2]], self.cache[self.memory[idxActual][2]]
+                
+                    self.memory[idxActual],self.memory[((idxActual + 1) * 2) - 1] = self.memory[((idxActual + 1) * 2) - 1], self.memory[idxActual]
+                    idxActual = ((idxActual + 1) * 2) - 1
         else:
-            return
+            return -1
     
     def print(self):
         return self.memory
@@ -71,29 +112,39 @@ newCache.get(41, 67)
 time.sleep(1)
 newCache.get(59, 0)
 time.sleep(1)
+
+# print(newCache.print())
+# print(newCache.cache)
+
+# print("Actualizar")
+# newCache.update(34)
+# newCache.update(23)
+# newCache.update(32)
+# newCache.update(56)
+# newCache.update(65)
+# newCache.update(41)
+# print(newCache.print())
+# print(newCache.cache)
+
 print(newCache.print())
+print(newCache.cache)
 newCache.get(1, 67)
 print(newCache.print())
+print(newCache.cache)
 newCache.get(2, 59)
 print(newCache.print())
+print(newCache.cache)
 newCache.get(868, 23)
 print(newCache.print())
+print(newCache.cache)
 newCache.get(432, 61)
 print(newCache.print())
+print(newCache.cache)
 newCache.get(62, 84)
 print(newCache.print())
+print(newCache.cache)
 newCache.get(29, 405)
 print(newCache.print())
 
+print(newCache.cache)
 
-
-# cash = dict()
-# cash[1] = "Homero"
-# print(cash)
-
-# casa = ["XD", "DX", "LA"]
-# casa.insert(0,"1")
-
-# print(casa)
-
-# print(casa.pop(0))
---
+++ Cache/prueba.py
@@ -0,0 +1,8 @@
+
+map = dict()
+
+map["LALA"] = "Blur"
+map["WAZA"] = "xd"
+
+map["LALA"], map["WAZA"] = map["WAZA"],map["LALA"]
+print(map)
